c0652860ecfb9c58e6bd167348ba9c0719f65841
Sebastien <muchhus@gmail.com>
2019-05-29 16:07:20 +0200

    exo de benoit

---
+++ Enonces-FormationJ2EE/Exo01-Banque Servlet/en01.servlet/src/main/java/fr/web/ServletClient.java
@@ -0,0 +1,30 @@
+package fr.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet qui va afficher tous les clients. <br/>
+ */
+@WebServlet(description = "Affiche tous les clients", urlPatterns = { "/ServletClient" })
+public class ServletClient extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructeur.
+	 */
+	public ServletClient() {
+		super();
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Code me en faisant usage de fr.bd.AccesBD
+	}
+}
---
+++ Enonces-FormationJ2EE/Exo01-Banque Servlet/en01.servlet/src/main/java/fr/web/ServletCompte.java
@@ -0,0 +1,31 @@
+
+package fr.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet qui va afficher tous les comptes d'un client. <br/>
+ */
+@WebServlet(description = "Affiche tous les comptes d'un client", urlPatterns = { "/ServletCompte" })
+public class ServletCompte extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Constructeur.
+	 */
+	public ServletCompte() {
+		super();
+	}
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Code me en faisant usage de fr.bd.AccesBD
+	}
+}
